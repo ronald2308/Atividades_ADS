98c5d96f0bc62b9124451cee880979a872c0b950
ronald2308 <143020055+ronald2308@users.noreply.github.com>
2023-09-08 23:38:47 -0300

    Atv-Projeto-11-09-23

---
+++ atv/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import facebook from "./img/facebook.svg"
+
+function App() {
+  return (
+
+    <div className="App">
+     
+     <header>
+
+      <img src={facebook} alt="logo do facebook"/>
+     
+     </header>
+        
+        <form>
+            
+            <p>Entrar no Facebook</p>
+           <input type="text" class="login" placeholder="E-mail ou telefone"/>
+           <input type="password" class="senha" placeholder="Senha"/>
+           <input type="text" class="entrar" placeholder="Entrar"/>
+           <a href="a">Esqueceu a conta?</a>
+           <button class="criar-conta">Criar nova conta</button>
+                   
+          </form>
+  
+     </div>
+  );
+}
+
+export default App;
